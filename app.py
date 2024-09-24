668ad9849446728636fe9af96a828204d2f3617d
Lee Shen Kai <shenkai.lee@winsys.com.sg>
2024-09-24 17:39:31 +0800

    fixed bugs

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from azure.iot.device.aio import IoTHubDeviceClient
 import os
-from dotenv import load_dotenv
-
 import random
 import datetime
-import time
 import json
 import asyncio
+
+from dotenv import load_dotenv
+from azure.iot.device.aio import IoTHubDeviceClient
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get the Azure IoT Hub connection string from the environment variable
+# Get the Azure IoT Hub device primary connection string from the environment variable
 connectionString = os.getenv("IOT_CONNECTION_STRING")
 
 async def sendToIotHub(data):
@@ -23,29 +22,27 @@ async def sendToIotHub(data):
         await device_client.connect()
 
         #Send the message
-        device_client.send_message(data)
+        await device_client.send_message(data)
         print("Message sent to IoT Hub:", data)
 
         # Shutdown the client
         await device_client.shutdown()
         
-    
     except Exception as e:
         print("Error:", str(e))
 
-def main():
-    # Run an infinite while loop to send data every 5 seconds
+async def main():
     while True:
         # Generate random value
         water_level = random.randint(0, 10)
-        # Generate data packet
-        data={
-            "device_id":"test-device-1",
-            "water_level":water_level,
-            "edge_time_stamp":str(datetime.datetime.now())
+        # Generate JSON data packet
+        data = {
+            "device_id": "test-device-1",
+            "water_level": water_level,
+            "edge_time_stamp": str(datetime.datetime.now())
         }
-        asyncio.run(sendToIotHub(data=json.dumps(data)))
-        time.sleep(10)
+        await sendToIotHub(data=json.dumps(data))  # Await the sendToIotHub call
+        await asyncio.sleep(10)  # asyncio.sleep for time interval 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())  # Run the main routine
